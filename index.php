5fe71ab2ccdf009e62737f44486ae9db6e77ec5b
usagi123 <maipham.quanghuy@gmail.com>
2019-10-14 22:29:40 +0700

    readding wp_title

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
         <div class="container-fluid custom-title">
             <div class="container">
-                <!-- <h1><?php wp_title(''); ?></h1> -->
+                <h1><?php wp_title(''); ?></h1>
             </div>
         </div>
         
