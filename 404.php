352fbe1049f3ca7d08b1ace535278ce4e87d57be
usagi123 <maipham.quanghuy@gmail.com>
2019-10-15 23:44:46 +0700

    add 404.php

---
+++ 404.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>404 - Page not found</title>
+    <link rel="icon" href="https://i.imgur.com/Gcdx3Dq.jpg">
+
+    <!-- Global Site Tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-113464551-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-113464551-1');
+    </script>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+
+    <!-- Font Awesome Icon CSS -->
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="<?php echo get_template_directory_uri() ?>'/css/404.css'" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- Body contents -->
+    <div class="masthead d-flex">
+        <div class="container text-center my-auto">
+        <div class="col-xl-8 col-centered">
+            <img src="https://i.imgur.com/HXpuBMd.gif" class="img-fluid" alt="" data-toggle="tooltip" data-placement="top" title="me irl">
+            <!-- <h1>The easiest and powerful way</h1> -->
+            <h1>Page not found</h1>
+            <h4><span data-toggle="tooltip" data-placement="top" title="oof">Sorry for this inconvenicence</span></h4>
+            <p>
+            Codes will finish by itself while I am sleeping... I think
+            </p>
+            <a href="javascript:history.back()"><button type="button" class="btn btn-outline-info">Back to previous page</button></a>
+        </div>
+        </div>
+    </div>
+    <!-- Bootstrap core JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="index.js"></script>
+
+</body>
+
+</html>
