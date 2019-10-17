a00da090ff0c759966b312d77bbdc55d59e6b31d
usagi123 <maipham.quanghuy@gmail.com>
2019-10-18 00:41:55 +0700

    change leave a reply w/o login form to bootstrap4

--- comments.php
+++ comments.php
@@ -33,7 +33,7 @@ if(post_password_required()) {
             'reply_text'        =>  'Reply',
             'page'              =>  '',
             'per_page'          =>  '',
-            'avatar'            =>  32, //avatar size
+            'avatar'            =>  50, //avatar size
             'reverse_top_level' =>  true, //top == new, bottom == old
             'reverse_children'  =>  true, //same like reverse top level
             'format'            =>  'html5',
@@ -51,5 +51,41 @@ if(post_password_required()) {
 
         <?php endif; ?>
     <?php endif; ?>
-    <?php comment_form(); ?>
+
+    <?php 
+        $args = array(
+            'fields' => apply_filters(
+                'comment_form_default_fields', array(
+                    'author' => '<div class="form-group comment-form-author">' .
+                                    '<label for="author">' . __( 'Name:' ) . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
+                                    '<input id="author" name="author" type="text" class="form-control" value="' . esc_attr( $commenter['comment_author'] ) . '"' . $aria_req . ' />'.
+                                '</div>',
+                    'email'  => '<div class="form-group comment-form-email">' . 
+                                    '<label for="email">' . __( 'Email:' ) . '</label> ' . ( $req ? '<span class="required">*</span>' : '' ) .
+                                    '<input id="email" name="email" type="text" class="form-control" value="' . esc_attr( $commenter['comment_author_email'] ) . '"' . $aria_req . ' />'.
+                                '</div>',
+                    'url'   =>  '<div class="form-group comment-form-url">' . 
+                                    '<label for="url">' . __( 'Website:' ) . '</label> ' .
+                                    '<input id="url" name="url" type="text" class="form-control" value="' . esc_attr( $commenter['comment_author_url'] ) . '"' . ' />'.
+                                '</div>',
+                    'cookies' =>    '<div class="form-check comment-form-cookies-consent">
+                                        <input id="wp-comment-cookies-consent" class="form-check-input" name="wp-comment-cookies-consent" type="checkbox" value="yes"' . $consent . ' />' . 
+                                        '<label class="form-check-label" for="wp-comment-cookies-consent">' . 
+                                            __( 'Save my name, email, and website in this browser for the next time I comment.' ) . 
+                                        '</label>
+                                    </div>',
+                )
+            ),
+            'comment_field' =>  '<div class="form-group comment-form-comment">' .
+                                '<label for="comment">' 
+                                . __( 'Comment:' ) . 
+                                '</label>' .
+                                '<textarea class="form-control" rows="5" id="comment" name="comment" aria-required="true"></textarea>' .
+                                '</div>',
+            'comment_notes_after' => '',
+            'title_reply' => 'Leave a Reply',
+            'class_submit' => 'btn btn-outline-info continue-reading float-left submit',
+        );
+    ?>
+    <?php comment_form($args); ?>
 </div>
