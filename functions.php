d2636085e4c8a528d9955ec5c2f373425454f4ff
usagi123 <maipham.quanghuy@gmail.com>
2019-11-30 11:43:13 +0700

    Remove increase upload limit error

--- functions.php
+++ functions.php
@@ -152,10 +152,5 @@ function widget_setup() {
 	);
 }
 add_action('widgets_init','widget_setup')
-
-//Increase upload size
-@ini_set( 'upload_max_size' , '64M' );
-@ini_set( 'post_max_size', '64M');
-@ini_set( 'max_execution_time', '300' );
 ?>
 
